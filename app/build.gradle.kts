d17b7ae9d1e693856b37fb3354452eb164c28e5e
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-20 17:51:39 -0500

    Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.activity.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
