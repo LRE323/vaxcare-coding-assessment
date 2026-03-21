be40b66592d80ff74f709b51cc0e32fdea51d1c7
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-20 21:45:15 -0500

    Fetching books

--- app/src/main/java/com/example/vaxcare/ListViewModel.kt
+++ app/src/main/java/com/example/vaxcare/ListViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.vaxcare
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vaxcare.models.Book
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,9 +14,36 @@ class ListViewModel @Inject constructor(
     private val bookRepository: BookRepository
 ) : ViewModel() {
     val uiState = MutableStateFlow(ListScreenUiState())
+
+    init {
+        fetchBooks()
+    }
+
+    private fun fetchBooks() {
+        handleLoading(true)
+
+        viewModelScope.launch {
+
+            uiState.update {
+                it.copy(
+                    bookList = bookRepository.fetchBookList()
+                )
+            }
+
+            handleLoading(false)
+        }
+    }
+
+    private fun handleLoading(shouldShowLoading: Boolean) {
+        uiState.update {
+            it.copy(shouldShowLoading = shouldShowLoading)
+        }
+    }
+
 }
 
 data class ListScreenUiState(
     val screenTitle: String = "",
-    val shouldShowLoading: Boolean = false
+    val shouldShowLoading: Boolean = false,
+    val bookList: List<Book>? = null
 )
