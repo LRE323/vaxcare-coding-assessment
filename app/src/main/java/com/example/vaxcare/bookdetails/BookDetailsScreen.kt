8ad878c77738786424080383ce69568c8ddd0537
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-20 23:52:23 -0500

    Adding Scaffold and Column to details screen

--- app/src/main/java/com/example/vaxcare/bookdetails/BookDetailsScreen.kt
+++ app/src/main/java/com/example/vaxcare/bookdetails/BookDetailsScreen.kt
@@ -1,9 +1,18 @@
 package com.example.vaxcare.bookdetails
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BookDetailsScreen() {
-    Text("BOOK DETAILS SCREEN")
+    Scaffold { paddingValues ->
+        Column(Modifier.padding(paddingValues).padding(16.dp)) {
+            Text("BOOK DETAILS SCREEN")
+        }
+    }
 }
