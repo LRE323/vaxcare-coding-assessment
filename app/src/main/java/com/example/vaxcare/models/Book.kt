679e977e5559e2ba29301b90f6861e16d99bf13c
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-21 00:29:03 -0500

    Using database constants object

--- app/src/main/java/com/example/vaxcare/models/Book.kt
+++ app/src/main/java/com/example/vaxcare/models/Book.kt
@@ -4,10 +4,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.example.vaxcare.room.BookStatusConverter
+import com.example.vaxcare.room.DatabaseConstants
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Entity(tableName = "books")
+@Entity(tableName = DatabaseConstants.BOOK_TABLE)
 @TypeConverters(BookStatusConverter::class)
 data class Book(
     @PrimaryKey val id: Int,
