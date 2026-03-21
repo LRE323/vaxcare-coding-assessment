c0450b9574c0cb1d7456e775a880bb47e7eec1fd
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-21 00:14:29 -0500

    Modifying data class for Room usage

--- app/src/main/java/com/example/vaxcare/models/Book.kt
+++ app/src/main/java/com/example/vaxcare/models/Book.kt
@@ -1,10 +1,16 @@
 package com.example.vaxcare.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.vaxcare.room.BookStatusConverter
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Entity(tableName = "books")
+@TypeConverters(BookStatusConverter::class)
 data class Book(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val title: String,
     val author: String,
     val status: BookStatus,
