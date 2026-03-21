12f04ec9b962d338b8d5137c13b3546e56c66a8b
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-20 21:01:02 -0500

    Creating UI state for List Screen

--- app/src/main/java/com/example/vaxcare/ListViewModel.kt
+++ app/src/main/java/com/example/vaxcare/ListViewModel.kt
@@ -2,8 +2,14 @@ package com.example.vaxcare
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @HiltViewModel
 class ListViewModel: ViewModel() {
-    val title = "LIST SCREEN"
+    val uiState = MutableStateFlow(ListScreenUiState())
 }
+
+data class ListScreenUiState(
+    val screenTitle: String = "",
+    val shouldShowLoading: Boolean = false
+)
