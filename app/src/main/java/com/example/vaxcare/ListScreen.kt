cdaf099bf1867fb87f367017396f9fbb33e9028a
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2026-03-20 21:01:50 -0500

    Collecting UI state

--- app/src/main/java/com/example/vaxcare/ListScreen.kt
+++ app/src/main/java/com/example/vaxcare/ListScreen.kt
@@ -3,21 +3,21 @@ package com.example.vaxcare
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
 fun ListScreen(
-    viewModel: ListViewModel = hiltViewModel()
+    viewModel: ListViewModel = hiltViewModel(),
+    uiState: ListScreenUiState = viewModel.uiState.collectAsStateWithLifecycle().value
 ) {
     Scaffold {
         Column(
             modifier = Modifier.padding(it).padding(16.dp)
         ) {
-            Text(text = viewModel.title)
         }
     }
 }
